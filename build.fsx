397ac0a57d1bfe8b02c9674939b77374039d71b9
Johannes Gustafsson <johannesgu@gmail.com>
2017-03-29 20:05:22 +0200

    mono

--- build.fsx
+++ build.fsx
@@ -1,15 +1,21 @@
 // include Fake lib
-#r @"packages\FAKE\tools\FakeLib.dll"
+// #r @".\packages\FAKE\tools\FakeLib.dll"
+#r "./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
 open Fake.Testing
 open Fake.FuchuHelper
 
 // Directories
-let buildDir  = @".\build\"
-let testDir   = @".\test\"
-let deployDir = @".\deploy\"
-let packagesDir = @".\packages"
+let buildDir  = "./build/"
+let testDir   = "./test/"
+let deployDir = "./deploy/"
+let packagesDir = "./packages"
+
+// Filesets
+let appReferences  =
+    !! "src/**/*.fsproj"
+      -- "src/**/*.Tests.fsproj"
 
 // Default target
 Target "Default" (fun _ ->
@@ -20,6 +26,10 @@ Target "Compile" (fun _ ->
     !! @"lsharp.sln"
         |> MSBuildRelease buildDir "Build"
         |> Log "AppBuild-Output: "
+
+    // compile all projects below src/app/
+    // MSBuildDebug buildDir "Build" appReferences
+    //     |> Log "AppBuild-Output: "
 )
 
 Target "Test" (fun _ ->
